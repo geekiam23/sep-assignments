df68c729698d7457bbde55453d26fa8dfba47980
Will Larry <will23larry@gmail.com>
2017-10-04 15:31:03 -0500

    Passed all of the tests

--- 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
+++ 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
@@ -8,11 +8,18 @@ class MyQueue
   end
 
   def enqueue(element)
+    @queue[@queue.length] = element
+    self.head = @queue[0]
+    self.tail = @queue[@queue.length - 1]
   end
 
   def dequeue
+    temp = @queue[0]
+    @queue.shift
+    temp
   end
 
   def empty?
+    @queue.length == 0
   end
 end
--- 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
+++ 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
@@ -7,11 +7,20 @@ class MyStack
   end
 
   def push(item)
+    @stack[@stack.length] = item
+    self.top = @stack[@stack.length - 1]
   end
 
   def pop
+    itemdDeleted = @stack.delete_at(@stack.length - 1)
+    self.top = @stack[@stack.length - 1]
+    itemdDeleted
   end
 
   def empty?
+    @stack.length == 0
   end
 end
+
+
+# ["Ben", "Rob"]
