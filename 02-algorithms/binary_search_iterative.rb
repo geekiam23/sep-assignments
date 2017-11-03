71f4983b9d0de2b43b121dafa7a90f693a254a18
William Larry <will23larry@gmail.com>
2017-11-03 16:38:10 -0500

    Finished the Iterative Binary Search

---
+++ 02-algorithms/binary_search_iterative.rb
@@ -0,0 +1,19 @@
+Class BinarySearch
+  def IterativeBinarySearch(collection, value)
+    low = 0
+    high = collection.last
+
+    while low <= high
+      mid = (low + high) / 2
+
+      if collection[mid] > value
+        high = mid - 1
+      elsif collection[mid] < value
+        low = mid -1
+      else
+        return mid
+      end
+    end
+    return "Value not found!"
+  end
+end
